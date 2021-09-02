231fa2ffb37bd7d882a8bc4698024e1fca8393a1
tamanugi <tamanugi@gmail.com>
2021-09-02 21:53:16 +0900

    chapter.3  mix phx.gen.schema Blogs.Tag tags tag:string

---
+++ priv/repo/migrations/20210902125254_create_tags.exs
@@ -0,0 +1,11 @@
+defmodule RealworldPhoenixLiveview.Repo.Migrations.CreateTags do
+  use Ecto.Migration
+
+  def change do
+    create table(:tags) do
+      add :tag, :string
+
+      timestamps()
+    end
+  end
+end
