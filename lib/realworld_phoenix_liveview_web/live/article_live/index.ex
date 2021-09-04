97290c23483a0e05d44c737bd5df2c37cba9c631
tamanugi <tamanugi@gmail.com>
2021-09-04 22:57:07 +0900

    chapter.5 listing articles

--- lib/realworld_phoenix_liveview_web/live/article_live/index.ex
+++ lib/realworld_phoenix_liveview_web/live/article_live/index.ex
@@ -2,7 +2,7 @@ defmodule RealworldPhoenixLiveviewWeb.ArticleLive.Index do
   use RealworldPhoenixLiveviewWeb, :live_view
 
   alias RealworldPhoenixLiveview.Blogs
-  alias RealworldPhoenixLiveview.Blogs.Article
+  alias RealworldPhoenixLiveview.Repo
 
   @impl true
   def mount(_params, _session, socket) do
@@ -14,33 +14,13 @@ defmodule RealworldPhoenixLiveviewWeb.ArticleLive.Index do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Article")
-    |> assign(:article, Blogs.get_article!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Article")
-    |> assign(:article, %Article{})
-  end
-
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "Listing Articles")
     |> assign(:article, nil)
   end
 
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    article = Blogs.get_article!(id)
-    {:ok, _} = Blogs.delete_article(article)
-
-    {:noreply, assign(socket, :articles, list_articles())}
-  end
-
   defp list_articles do
-    Blogs.list_articles()
+    Blogs.list_articles() |> Repo.preload(:author)
   end
 end
