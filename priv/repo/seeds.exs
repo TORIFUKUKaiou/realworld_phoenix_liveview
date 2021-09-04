92873eb9f2b699322990f288b0326fbe75237552
tamanugi <tamanugi@gmail.com>
2021-09-04 23:19:53 +0900

    fix seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,8 +14,16 @@ alias RealworldPhoenixLiveview.Blogs
 alias RealworldPhoenixLiveview.Blogs.Article
 alias RealworldPhoenixLiveview.Blogs.Comment
 alias RealworldPhoenixLiveview.Blogs.Tag
+
+alias RealworldPhoenixLiveview.Accounts
+alias RealworldPhoenixLiveview.Accounts.User
+
 alias RealworldPhoenixLiveview.Repo
 
-article =
-  %{title: "some title", body: "some body", tags: ["tag1", "tag2"]}
-  |> Blogs.create_article()
+{:ok, author} = Accounts.register_user(%{email: "hoge@test.com", password: "validpassword"})
+
+%{title: "some title1", body: "some body", tags: ["tag1", "tag2"], author_id: author.id}
+|> Blogs.create_article()
+
+%{title: "some title2", body: "some body", tags: ["tag2", "tag3"], author_id: author.id}
+|> Blogs.create_article()
